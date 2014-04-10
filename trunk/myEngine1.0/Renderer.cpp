06b4fd6ac2f898575ba7a8d8b00dfe0a5918163e
luckystrike9422@gmail.com <luckystrike9422@gmail.com@560311cc-f9ff-04aa-4859-a6eff04a4cba>
2014-04-10 00:08:01 +0000

    Error De Linkeo.

---
+++ trunk/myEngine1.0/Renderer.cpp
@@ -0,0 +1,43 @@
+#include "Renderer.h"
+
+Renderer::Renderer():
+d3d(new LPDIRECT3D9()),
+d3d_dev(new LPDIRECT3DDEVICE9()){
+	// Again, Nothing to do.
+}
+
+Renderer::~Renderer(){
+	(*d3d)->Release();
+	delete d3d;
+	(*d3d_dev)->Release();
+	delete d3d_dev;
+}
+
+bool Renderer::Init(HWND _HwnD){
+	*d3d = Direct3DCreate9(D3D_SDK_VERSION);
+	D3DPRESENT_PARAMETERS d3dpp;
+	ZeroMemory(&d3dpp, sizeof(d3dpp));
+	d3dpp.Windowed = TRUE;
+	d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;
+	d3dpp.hDeviceWindow = _HwnD;
+	if((*d3d)->CreateDevice( D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, _HwnD, D3DCREATE_SOFTWARE_VERTEXPROCESSING, &d3dpp, d3d_dev) != D3D_OK)
+		return false;
+	return true;
+}
+
+void Renderer::BeginFrame(){
+	(*d3d_dev)->Clear(0, NULL, D3DCLEAR_TARGET, D3DCOLOR_XRGB(0,0,255), 1.0f, 0);
+	(*d3d_dev)->BeginScene();
+}
+
+void Renderer::EndFrame(){
+	(*d3d_dev)->EndScene();
+	(*d3d_dev)->Present(NULL,NULL,NULL,NULL);
+}
+
+LPDIRECT3D9 Renderer::Getd3d(){
+	return *d3d;
+}
+LPDIRECT3DDEVICE9 Renderer::Getd3ddev(){
+	return *d3d_dev;
+}
--- trunk/myEngine1.0/Window.cpp
+++ trunk/myEngine1.0/Window.cpp
@@ -1,82 +1,35 @@
 #include "Window.h"
 #include <string>
-using namespace dmr;
-const char * dmr::Window::m_pszClassName = "Window";
-Window * Window::CrearVentana( HINSTANCE hInst ,int nCmdShow, wstring _t, int _w, int _h) {
 
-	WNDCLASS wincl = {0};
-    if (!GetClassInfo(hInst, m_pszClassName, &wincl)) {
-        wincl.hInstance = hInst;
-        wincl.lpszClassName = m_pszClassName;
-        wincl.lpfnWndProc = WindowProc; /*
-		wincl.cbClsExtra = 0;
-		wincl.cbWndExtra = 0;
-		wincl.hIcon = NULL;
-		wincl.hCursor = NULL;
-		wincl.lpszMenuName = NULL;
-		wincl.style = CS_HREDRAW | CS_VREDRAW; */
-        wincl.hbrBackground = (HBRUSH)(COLOR_BTNFACE+1);
-        if (RegisterClass(&wincl) == 0) {
-            MessageBox(NULL,"The window class failed to register.","Error",0);
-            return 0;
-        }
-    }
+Window::Window(HINSTANCE hInstance, char* cl):WC(new WNDCLASS),m_hWnd(NULL),_hInst(hInstance),m_pszClassName(cl){
+}
 
-    Window * pWindow = new Window;
-	/* (char * )_t.c_str() */
-	HWND hWnd = CreateWindow( m_pszClassName,(char * )_t.c_str(), WS_VISIBLE | WS_OVERLAPPEDWINDOW, 50, 50, _w + 8, _h + 30, NULL, NULL, hInst, pWindow );
-    if ( hWnd == NULL ) {
-        delete pWindow;
-        MessageBox(NULL,"Problem creating the window.","Error",0); 
-        return 0; 
-    }
+bool Window::CrearVentana(wstring _t, int _w, int _h) {
+	
+	ZeroMemory(WC, sizeof(*WC));
+	WC->lpfnWndProc=WindowProc;
+	WC->hInstance=_hInst;
+	WC->hbrBackground=(HBRUSH)(COLOR_BACKGROUND);
+	WC->lpszClassName=m_pszClassName;
+	if(!RegisterClass(WC)){return false;}
 
-	ShowWindow(hWnd,nCmdShow);
-    UpdateWindow(hWnd);
+	
+	m_hWnd = CreateWindow( m_pszClassName,(char * )_t.c_str(), WS_VISIBLE | WS_OVERLAPPEDWINDOW, 50, 50, _w + 8, _h + 30, NULL, NULL, _hInst, NULL );
+	if(m_hWnd != NULL) return true;
 
-	//
-	BOOL bRet;
-    MSG msg;
-    while( (bRet = GetMessage( &msg, 0, 0, 0 )) != 0){ 
-        if (bRet == -1){
-            // handle the error and possibly exit
-        }
-        else{
-            TranslateMessage(&msg); 
-            DispatchMessage(&msg); 
-        }
-    }
-
-
-    return pWindow;
-
+	return false;	
 }
 
  LRESULT CALLBACK Window::WindowProc( HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam ) {
-	 
-    switch ( uMsg ) {
-					
-        case WM_NCCREATE: {
-            CREATESTRUCT * pcs = (CREATESTRUCT*)lParam;
-            Window * pWindow = (Window*)pcs->lpCreateParams;
-            pWindow->m_hWnd = hwnd;
-            SetWindowLongPtr( hwnd, GWLP_USERDATA, (LONG)pcs->lpCreateParams );
-            return TRUE;
-        }
 
-        case WM_DESTROY: {
-            Window * pWindow = (Window *)GetWindowLongPtr( hwnd, GWLP_USERDATA );
-            if ( pWindow ) pWindow->m_hWnd = 0;
-			PostQuitMessage(0);
-            break;
-        }
+	 switch(uMsg){
+		case WM_CLOSE:
+				PostQuitMessage(0);
+				break;
+		default: return DefWindowProc(hwnd, uMsg, wParam, lParam);
+	 }
 
-        default:
-            return DefWindowProc( hwnd, uMsg, wParam, lParam );
-
-    }
-
-    return 0;
+	 return 0;
 
 }
 
